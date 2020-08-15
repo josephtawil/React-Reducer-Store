0ef8362d5d791976fc529ae9fd5b5a089934eaf7
Joseph <jtawil2015@gmail.com>
2020-08-15 12:38:21 -0800

    created list and created prioritize and delete function buttons

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,13 +10,16 @@ const Form = () => {
     function handleSubmit(e) {
         e.preventDefault();
         console.log(inputRef.current.value);
+
+        dispatch({type: "add", name: inputRef.current.value});
+        inputRef.current.value = "";
     }
 
     return (
         <div>
             <h1> Enter a todo </h1>
             <form className="form-group mt-5" onSubmit={handleSubmit}>
-                <input ref={inputRef} className="form-control"/>
+                <input ref={inputRef} className="form-control" placeholder="Enter todo text"/>
                 <button type="submit" className="btn btn-success mt-5">
                     Add a todo
                 </button>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import {useTodoContext} from "../utils/GlobalState";
+
 
 const TodoList = () => {
+
+    const [state,dispatch] = useTodoContext();
+
     return (
         <div>
-            <h1> Hello from TodoList </h1>
+            <h4> The list:  </h4>
+            <ul className="list-group">
+                
+                {state.map((item,index)=>{
+                    return(
+                        <li className="list-group-item" key={index}>
+                        <button onClick={()=>dispatch({type:"prioritize", index})} className="btn btn-warning mr-4">Prioritize</button>
+                        <button onClick={()=>dispatch({type:"remove", index})} className="btn btn-danger mr-4">Delete</button>
+                        <span className={item.priority ? "font-weight-bold" : ""}>{item.name}</span>
+                    </li>
+                    )
+                })};
+            </ul>
+
         </div>
     )
 }
